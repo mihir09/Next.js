bdda2707f94e87874a51447407ac5af89ae5cef7
mihir09 <mj7774542@gmail.com>
2023-09-24 17:44:26 -0700

    NextJS with Tailwind

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+// import styles from './ProductCard.module.css'
+
+const ProductCard = () => {
+  return (
+    // <div className={styles.card}>
+    // <div className='p-10 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>
+    <div>
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
