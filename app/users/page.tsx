6158f700b8dd03d1cca85f5a7f61d026e0a9da85
mihir09 <mj7774542@gmail.com>
2023-09-28 12:34:52 -0700

    programmatic navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import UserTable from './UserTable'
+import Link from 'next/link'
 
 interface Props {
     searchParams: { sortOrder: string }
 }
 
 const UsersPage =async ({ searchParams: {sortOrder}}: Props) => {
-    console.log(sortOrder)
+    // console.log(sortOrder)
 
     return (
         <>
             <h1>Users</h1>
             {/* <p>{new Date().toLocaleTimeString()}</p> */}
+            <Link href="/users/new" className='btn'>New Users</Link>
             <UserTable sortOrder={sortOrder}/>
         </>
     )
